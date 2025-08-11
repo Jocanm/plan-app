4e4b30e186f2d4705fbf657f8c4076a9c87b2f10
Jose Angarita <amjose09@gmail.com>
2025-08-11 17:31:47 -0500

    next auth changes and css classes

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,9 +4,25 @@ import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import prisma from "./prisma";
 
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
+}
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google, GitHub],
+  session: {
+    strategy: "database",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+    updateAge: 24 * 60 * 60, // 24 hours
+  },
   pages: {
     signIn: "/auth/login",
     error: "/auth/error",
