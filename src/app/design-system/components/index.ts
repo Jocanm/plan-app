ccb5632fe3a7091c8c1c317e3ebab3ce3165a743
Jose Angarita <amjose09@gmail.com>
2025-10-16 15:38:33 -0500

    fix: deleting ununsed components

--- src/app/design-system/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { InteractiveProjectSelector } from "./InteractiveProjectSelector";
-export { InteractiveTagInput } from "./InteractiveTagInput";
-export { InteractiveTaskCards } from "./InteractiveTaskCards";
