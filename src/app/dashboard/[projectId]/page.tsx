604102c97183698de440f37f7c03dfbc1a6afbcc
Jose Angarita <amjose09@gmail.com>
2025-10-22 14:48:02 -0500

    fix: unused var

--- src/app/dashboard/[projectId]/page.tsx
+++ src/app/dashboard/[projectId]/page.tsx
@@ -1,6 +1,5 @@
 const ProjectPage = async (props: PageProps<"/dashboard/[projectId]">) => {
   const { projectId } = await props.params;
-  const search = await props.searchParams;
 
   return <div>Project: {projectId}</div>;
 };
