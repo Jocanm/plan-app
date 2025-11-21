6625dafdebd64d4bb4608df7e3b97b83bec44328
Jose Angarita <amjose09@gmail.com>
2025-11-21 16:38:51 -0500

    feat: removing luxon

--- src/features/tasks/data/tasks.repository.ts
+++ src/features/tasks/data/tasks.repository.ts
@@ -2,7 +2,5 @@ import prisma from "@/lib/prisma";
 import { ITaskRepository } from "../domain/types/repository";
 
 export const createTask: ITaskRepository["createTask"] = async data => {
-  const taskCreated = await prisma.task.create({ data });
-
-  return taskCreated;
+  return await prisma.task.create({ data });
 };
