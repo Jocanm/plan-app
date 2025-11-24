dc589612de0a2040b43b27a1af8e1d0f8ac0cbd4
Jose Angarita <amjose09@gmail.com>
2025-11-24 12:50:16 -0500

    feat: custom task card animation

--- src/features/projects/components/tasks/ProjectTasks.tsx
+++ src/features/projects/components/tasks/ProjectTasks.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { TaskCard } from "@/features/tasks/components/card/TaskCard";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { useTranslations } from "next-intl";
 import { useProjectTasks } from "../providers/ProjectTasksProvider";
 import { NoTasks } from "./NoTasks";
@@ -8,6 +9,7 @@ import { NoTasks } from "./NoTasks";
 export const ProjectTasks = () => {
   const { tasks } = useProjectTasks();
   const t = useTranslations("project");
+  const [tasksParent] = useAutoAnimate();
 
   if (tasks.length === 0) {
     return <NoTasks />;
@@ -16,7 +18,7 @@ export const ProjectTasks = () => {
   return (
     <section className="mb-8">
       <h2 className="text-lg font-semibold mb-4">{t("tasks_heading")}</h2>
-      <ul className="space-y-4 mt-4">
+      <ul className="space-y-4 mt-4" ref={tasksParent}>
         {tasks.map(task => (
           <li key={task.id}>
             <TaskCard task={task} />
